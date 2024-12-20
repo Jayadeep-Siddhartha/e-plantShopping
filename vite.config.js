8a393691b21db1378e2aa23474e273e399b081f5
Jayadeep-Siddhartha <jayadeep.siddhartha@gmail.com>
2024-12-20 21:34:24 +0530

    Updated code

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
   plugins: [react()],
-})
+  server: {
+    open: true, // Automatically opens the browser on `npm run dev`
+  },
+  build: {
+    rollupOptions: {
+      input: 'index.html', // Correct the path to the entry point
+    },
+  },
+});
